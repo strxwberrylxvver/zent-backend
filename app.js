42ff3fc44aeed8896a0f07489365907b9189694c
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2025-12-31 06:32:39 +0000

    Refactoring: Using prepared statements and packaging SQL query strings with their data in all SQL queries

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import database from "./database.js";
 import cors from "cors";
-import { v4 as uuidv4 } from "uuid";
 
 const app = new express();
 
@@ -15,14 +14,21 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const read = async (sql, id) => {
+const read = async (query) => {
   try {
-    const [result] = await database.query(sql, {ID:id});
+    const [result] = await database.query(query.sql, query.data);
     return result.length === 0
       ? { isSuccess: false, result: null, message: "No record(s) found." }
-      : { isSuccess: true, result, message: "Record(s) successfully recovered.", };
+      : {
+          isSuccess: true,
+          result,
+          message: "Record(s) successfully recovered.",
+        };
   } catch (error) {
-    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}`,
+    return {
+      isSuccess: false,
+      result: null,
+      message: `Failed to execute query: ${error.message}`,
     };
   }
 };
@@ -34,14 +40,36 @@ const buildSetFields = (fields) =>
     "SET"
   );
 
-const createTransactions = async (sql, record) => {
+const createTransactions = async (createQuery) => {
   try {
-    const status = await database.query(sql, record);
-    const recoverRecordSql = buildTransactionsSelectSQL(
-      status[0].insertId,
+    const status = await database.query(createQuery.sql, createQuery.data);
+    const readQuery = buildTransactionsReadQuery(status[0].insertId, null);
+    const { isSuccess, result, message } = await read(readQuery);
+
+    return isSuccess
+      ? { isSuccess: true, result, message: "Record successfully recovered." }
+      : {
+          isSuccess: false,
+          result: null,
+          message: `Failed to recover inserted record : ${message}`,
+        };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      result: null,
+      message: `Failed to execute query: ${error.message}`,
+    };
+  }
+};
+
+const updateTransactions = async (updateQuery) => {
+  try {
+    const status = await database.query(updateQuery.sql, updateQuery.data);
+    const readQuery = buildTransactionsReadQuery(
+      updateQuery.data.TransactionID,
       null
     );
-    const { isSuccess, result, message } = await read(recoverRecordSql);
+    const { isSuccess, result, message } = await read(readQuery);
 
     return isSuccess
       ? { isSuccess: true, result, message: "Record successfully recovered." }
@@ -59,31 +87,9 @@ const createTransactions = async (sql, record) => {
   }
 };
 
-const modifyTransactions = async (sql, id, record) => {
+const deleteTransactions = async (deleteQuery) => {
   try {
-    const status = await database.query(sql, { ...record, TransactionID: id });
-    const recoverRecordSql = buildTransactionsSelectSQL(id, null);
-    const { isSuccess, result, message } = await read(recoverRecordSql);
-
-    return isSuccess
-      ? { isSuccess: true, result, message: "Record successfully recovered." }
-      : {
-          isSuccess: false,
-          result: null,
-          message: `Failed to recover inserted record : ${message}`,
-        };
-  } catch (error) {
-    return {
-      isSuccess: false,
-      result: null,
-      message: `Failed to execute query: ${error.message}`,
-    };
-  }
-};
-
-const deleteTransactions = async (sql, id) => {
-  try {
-    const status = await database.query(sql, { TransactionID: id });
+    const status = await database.query(deleteQuery.sql, deleteQuery.data);
 
     return status[0].affectedRows === 0
       ? {
@@ -105,46 +111,7 @@ const deleteTransactions = async (sql, id) => {
   }
 };
 
-const buildTransactionsModifySQL = (record) => {
-  const table = "transactions";
-
-  const mutableFields = [
-    "Name",
-    "Date",
-    "Amount",
-    "Category",
-    "PaymentMethod",
-    "UserID",
-  ];
-
-  const keys = mutableFields.filter((f) => record[f] !== undefined);
-  const setClause = keys.map((f) => `${f} = :${f}`).join(", ");
-
-  return `UPDATE ${table} SET ${setClause} WHERE TransactionID = :TransactionID`;
-};
-
-const buildTransactionsInsertSQL = () => {
-  const table = "transactions";
-  const fields = [
-    "TransactionID",
-    "Name",
-    "Date",
-    "Amount",
-    "Category",
-    "PaymentMethod",
-    "UserID",
-  ];
-
-  const columns = fields.join(", ");
-  const placeholders = fields.map((f) => `:${f}`).join(", ");
-
-  return `INSERT INTO ${table} (${columns}) VALUES (${placeholders})`;
-};
-const buildTransactionsDeleteSQL = () => {
-  return `DELETE FROM transactions WHERE TransactionID = :TransactionID`;
-};
-
-const buildTransactionsSelectSQL = (id, variant) => {
+const buildTransactionsReadQuery = (id, variant) => {
   let sql = "";
   let table =
     "transactions INNER JOIN users ON transactions.UserID=users.UserID";
@@ -166,13 +133,54 @@ const buildTransactionsSelectSQL = (id, variant) => {
       sql = `SELECT ${fields} FROM ${table} `;
       if (id) sql += ` WHERE TransactionID = :ID`;
   }
-  return sql;
+  return { sql, data: { ID: id } };
+};
+
+const buildTransactionsCreateQuery = (record) => {
+  const table = "transactions";
+  const fields = [
+    "TransactionID",
+    "Name",
+    "Date",
+    "Amount",
+    "Category",
+    "PaymentMethod",
+    "UserID",
+  ];
+
+  const columns = fields.join(", ");
+  const placeholders = fields.map((f) => `:${f}`).join(", ");
+  const sql = `INSERT INTO ${table} (${columns}) VALUES (${placeholders})`;
+  return { sql, data: record };
+};
+
+const buildTransactionsUpdateQuery = (record, id) => {
+  const table = "transactions";
+
+  const mutableFields = [
+    "Name",
+    "Date",
+    "Amount",
+    "Category",
+    "PaymentMethod",
+    "UserID",
+  ];
+
+  const keys = mutableFields.filter((f) => record[f] !== undefined);
+  const setClause = keys.map((f) => `${f} = :${f}`).join(", ");
+  const sql = `UPDATE ${table} SET ${setClause} WHERE TransactionID = :TransactionID`;
+  return { sql, data: { ...record, TransactionID: id } };
+};
+
+const buildTransactionsDeleteQuery = (id) => {
+  const sql = `DELETE FROM transactions WHERE TransactionID = :TransactionID`;
+  return { sql, data: {TransactionID:id} };
 };
 
 const getTransactionsController = async (req, res, variant) => {
   const id = req.params.id;
-  const sql = buildTransactionsSelectSQL(id, variant);
-  const { isSuccess, result, message: accessorMessage } = await read(sql, id);
+  const query = buildTransactionsReadQuery(id, variant);
+  const { isSuccess, result, message: accessorMessage } = await read(query);
   if (!isSuccess) return res.status(400).json({ message: accessorMessage });
 
   res.status(200).json(result);
@@ -180,16 +188,12 @@ const getTransactionsController = async (req, res, variant) => {
 
 const postTransactionsController = async (req, res) => {
   const record = req.body;
-
-  // const record = { TransactionID: id, ...req.body };
-  // const transactionId = uuidv4();
-  // const record = { TransactionID: transactionId, ...req.body };
-  const sql = buildTransactionsInsertSQL();
+  const query = buildTransactionsCreateQuery(record);
   const {
     isSuccess,
     result,
     message: accessorMessage,
-  } = await createTransactions(sql, record);
+  } = await createTransactions(query);
   if (!isSuccess) return res.status(404).json({ message: accessorMessage });
   res.status(201).json(result);
 };
@@ -198,12 +202,12 @@ const putTransactionsController = async (req, res) => {
   const id = req.params.id;
   const record = req.body;
 
-  const sql = buildTransactionsModifySQL(id, record);
+  const query = buildTransactionsUpdateQuery(record, id);
   const {
     isSuccess,
     result,
     message: accessorMessage,
-  } = await modifyTransactions(sql, id, record);
+  } = await updateTransactions(query);
 
   if (!isSuccess) return res.status(404).json({ message: accessorMessage });
   res.status(200).json(result);
@@ -212,12 +216,12 @@ const putTransactionsController = async (req, res) => {
 const deleteTransactionsController = async (req, res) => {
   const id = req.params.id;
 
-  const sql = buildTransactionsDeleteSQL();
+  const query = buildTransactionsDeleteQuery(id);
   const {
     isSuccess,
     result,
     message: accessorMessage,
-  } = await deleteTransactions(sql, id);
+  } = await deleteTransactions(query);
 
   if (!isSuccess) return res.status(404).json({ message: accessorMessage });
   res.status(200).json(result);
