d63ef5f3d4e056098d684723058ba20e645bd6dd
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2026-02-06 14:38:32 +0000

    created Accessor class to handle get, post, put and delete controllers

---
+++ controllers/Controller.js
@@ -0,0 +1,55 @@
+class Controller {
+  constructor(accessor) {
+    this.accessor = accessor;
+  }
+
+  get = async (req, res, variant) => {
+    const id = req.params.id;
+    const {
+      isSuccess,
+      result,
+      message: accessorMessage,
+    } = await this.accessor.read(id, variant);
+    if (!isSuccess) return res.status(400).json({ message: accessorMessage });
+
+    res.status(200).json(result);
+  };
+
+  post = async (req, res) => {
+    const record = req.body;
+    record.TransactionID = uuidv4();
+    const {
+      isSuccess,
+      result,
+      message: accessorMessage,
+    } = await this.accessor.create(record);
+    if (!isSuccess) return res.status(404).json({ message: accessorMessage });
+    res.status(201).json(result);
+  };
+
+  put = async (req, res) => {
+    const id = req.params.id;
+    const record = req.body;
+    const {
+      isSuccess,
+      result,
+      message: accessorMessage,
+    } = await this.accessor.update(record, id);
+
+    if (!isSuccess) return res.status(404).json({ message: accessorMessage });
+    res.status(200).json(result);
+  };
+
+  delete = async (req, res) => {
+    const id = req.params.id;
+    const {
+      isSuccess,
+      result,
+      message: accessorMessage,
+    } = await this.accessor.delete(id);
+
+    if (!isSuccess) return res.status(404).json({ message: accessorMessage });
+    res.status(200).json(result);
+  };
+}
+export default Controller;
--- routers/transactions-router.js
+++ routers/transactions-router.js
@@ -3,70 +3,22 @@ import database from "../database.js";
 import Model from "../models/Model.js";
 import modelConfig from "../models/transactions-model.js";
 import Accessor from "../accessors/Accessor.js";
+import Controller from "../controllers/Controller.js";
 import { v4 as uuidv4 } from "uuid";
 
 const router = Router();
-
 const model = new Model(modelConfig);
 const accessor = new Accessor(model, database);
-
-const getTransactionsController = async (req, res, variant) => {
-  const id = req.params.id;
-  const {
-    isSuccess,
-    result,
-    message: accessorMessage,
-  } = await accessor.read(id, variant);
-  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
-
-  res.status(200).json(result);
-};
-
-const postTransactionsController = async (req, res) => {
-  const record = req.body;
-  record.TransactionID = uuidv4();
-  const {
-    isSuccess,
-    result,
-    message: accessorMessage,
-  } = await accessor.create(record);
-  if (!isSuccess) return res.status(404).json({ message: accessorMessage });
-  res.status(201).json(result);
-};
-
-const putTransactionsController = async (req, res) => {
-  const id = req.params.id;
-  const record = req.body;
-  const {
-    isSuccess,
-    result,
-    message: accessorMessage,
-  } = await accessor.update(record, id);
-
-  if (!isSuccess) return res.status(404).json({ message: accessorMessage });
-  res.status(200).json(result);
-};
-
-const deleteTransactionsController = async (req, res) => {
-  const id = req.params.id;
-  const {
-    isSuccess,
-    result,
-    message: accessorMessage,
-  } = await accessor.delete(id);
-
-  if (!isSuccess) return res.status(404).json({ message: accessorMessage });
-  res.status(200).json(result);
-};
+const controller = new Controller(accessor);
 
 router.get("/users/:id", (req, res) =>
   getTransactionsController(req, res, "user")
 );
-router.get("/:id", (req, res) => getTransactionsController(req, res, null));
-router.get("/", (req, res) => getTransactionsController(req, res, null));
+router.get("/:id", (req, res) => controller.get(req, res, null));
+router.get("/", (req, res) => controller.get(req, res, null));
 
-router.post("/", postTransactionsController);
-router.put("/:id", putTransactionsController);
-router.delete("/:id", deleteTransactionsController);
+router.post("/", controller.post);
+router.put("/:id", controller.put);
+router.delete("/:id", controller.delete);
 
 export default router;
