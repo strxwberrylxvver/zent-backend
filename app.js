a1a3a7688c0c4e2f8ab93a0e07de1dee6ee38f8e
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2026-03-14 21:53:51 +0000

    fix: collective router debugged

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import transactionsConfig from "./models/transactions-model.js";
 import goalsConfig from "./models/goals-model.js";
 import budgetsConfig from "./models/budgets-model.js";
+import collectiveRouter from "./routers/collective-router.js";
 
 const app = express();
 
@@ -16,9 +17,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/transactions", createRouter(transactionsConfig));
-app.use("/api/savingsgoals", createRouter(goalsConfig));
-app.use("/api/budgets", createRouter(budgetsConfig));
+app.use("/api/transactions", collectiveRouter(transactionsConfig));
+app.use("/api/savingsgoals", collectiveRouter(goalsConfig));
+app.use("/api/budgets", collectiveRouter(budgetsConfig));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
